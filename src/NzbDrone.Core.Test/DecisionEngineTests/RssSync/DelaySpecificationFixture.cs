Sonarr/Sonarr

New: Allow user to choose whether delay profile should apply to release of the highest enabled quality

src/NzbDrone.Core.Test/DecisionEngineTests/RssSync/DelaySpecificationFixture.cs
@@ -124,8 +124,19 @@ namespace NzbDrone.Core.Test.DecisionEngineTests.RssSync
         }
 
         [Test]
-        public void should_be_true_when_quality_and_language_is_last_allowed_in_profile()
+        public void should_be_false_when_quality_and_language_is_last_allowed_in_profile_and_bypass_disabled()
         {
+            _remoteEpisode.ParsedEpisodeInfo.Quality = new QualityModel(Quality.Bluray720p);
+            _remoteEpisode.ParsedEpisodeInfo.Language = Language.French;
+
+            Subject.IsSatisfiedBy(_remoteEpisode, null).Accepted.Should().BeTrue();
+        }
+
+        [Test]
+        public void should_be_true_when_quality_and_language_is_last_allowed_in_profile_and_bypass_enabled()
+        {
+            _delayProfile.BypassIfHighestQuality = true;
+
             _remoteEpisode.ParsedEpisodeInfo.Quality = new QualityModel(Quality.Bluray720p);
             _remoteEpisode.ParsedEpisodeInfo.Language = Language.French;
 

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/DelaySpecification.cs
@@ -67,14 +67,17 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
             }
 
             // If quality meets or exceeds the best allowed quality in the profile accept it immediately
-            var bestQualityInProfile = qualityProfile.LastAllowedQuality();
-            var isBestInProfile = qualityComparer.Compare(subject.ParsedEpisodeInfo.Quality.Quality, bestQualityInProfile) >= 0;
-            var isBestInProfileLanguage = languageComparer.Compare(subject.ParsedEpisodeInfo.Language, languageProfile.LastAllowedLanguage()) >= 0;
-
-            if (isBestInProfile && isBestInProfileLanguage && isPreferredProtocol)
+            if (delayProfile.BypassIfHighestQuality)
             {
-                _logger.Debug("Quality and language is highest in profile for preferred protocol, will not delay");
-                return Decision.Accept();
+                var bestQualityInProfile = qualityProfile.LastAllowedQuality();
+                var isBestInProfile = qualityComparer.Compare(subject.ParsedEpisodeInfo.Quality.Quality, bestQualityInProfile) >= 0;
+                var isBestInProfileLanguage = languageComparer.Compare(subject.ParsedEpisodeInfo.Language, languageProfile.LastAllowedLanguage()) >= 0;
+
+                if (isBestInProfile && isBestInProfileLanguage && isPreferredProtocol)
+                {
+                    _logger.Debug("Quality and language is highest in profile for preferred protocol, will not delay");
+                    return Decision.Accept();
+                }
             }
 
             var episodeIds = subject.Episodes.Select(e => e.Id);

christianfds/Radarr

(cherry picked from commit d668e923af83ab7f1589d947fec9f40a3919e0b8)

src/NzbDrone.Core.Test/DecisionEngineTests/RssSync/DelaySpecificationFixture.cs
@@ -104,8 +104,20 @@ namespace NzbDrone.Core.Test.DecisionEngineTests.RssSync
         }
 
         [Test]
-        public void should_be_true_when_quality_is_last_allowed_in_profile()
+        public void should_be_false_when_quality_is_last_allowed_in_profile_and_bypass_disabled()
         {
+            _remoteMovie.ParsedMovieInfo.Quality = new QualityModel(Quality.Bluray720p);
+
+            _remoteMovie.Release.PublishDate = DateTime.UtcNow;
+
+            Subject.IsSatisfiedBy(_remoteMovie, null).Accepted.Should().BeFalse();
+        }
+
+        [Test]
+        public void should_be_true_when_quality_is_last_allowed_in_profile_and_bypass_enabled()
+        {
+            _delayProfile.BypassIfHighestQuality = true;
+
             _remoteMovie.ParsedMovieInfo.Quality = new QualityModel(Quality.Bluray720p);
 
             Subject.IsSatisfiedBy(_remoteMovie, null).Accepted.Should().BeTrue();

src/NzbDrone.Core/DecisionEngine/Specifications/RssSync/DelaySpecification.cs
@@ -1,4 +1,3 @@
-using System.Linq;
 using NLog;
 using NzbDrone.Core.CustomFormats;
 using NzbDrone.Core.Download.Pending;
@@ -78,13 +77,16 @@ namespace NzbDrone.Core.DecisionEngine.Specifications.RssSync
             }
 
             // If quality meets or exceeds the best allowed quality in the profile accept it immediately
-            var bestQualityInProfile = profile.LastAllowedQuality();
-            var isBestInProfile = comparer.Compare(subject.ParsedMovieInfo.Quality.Quality, bestQualityInProfile) >= 0;
-
-            if (isBestInProfile && isPreferredProtocol)
+            if (delayProfile.BypassIfHighestQuality)
             {
-                _logger.Debug("Quality is highest in profile for preferred protocol, will not delay.");
-                return Decision.Accept();
+                var bestQualityInProfile = profile.LastAllowedQuality();
+                var isBestInProfile = comparer.Compare(subject.ParsedMovieInfo.Quality.Quality, bestQualityInProfile) >= 0;
+
+                if (isBestInProfile && isPreferredProtocol)
+                {
+                    _logger.Debug("Quality is highest in profile for preferred protocol, will not delay.");
+                    return Decision.Accept();
+                }
             }
 
             var oldest = _pendingReleaseService.OldestPendingRelease(subject.Movie.Id);